76a0814b983ae598579e12dfdb297cc3cdb7628b
Diego Muracciole <diegomuracciole@gmail.com>
2024-04-30 00:38:54 +0200

    feat: concatenate short segments for better text output

--- src/processes/tts/model.py
+++ src/processes/tts/model.py
@@ -6,12 +6,29 @@ cwd = os.path.dirname(__file__)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+def concatenate_to_segments(strings, limit=5):
+  result = []
+  current_segment = ""
+
+  for s in strings:
+    if len(current_segment) + len(s) > limit:
+      if current_segment != '': result.append(current_segment)
+      current_segment = s
+    else:
+      current_segment += f' {s}'
+
+  # Append the last segment if it's not empty
+  if current_segment:
+    result.append(current_segment)
+
+  return result
 class TTSModel:
   def __init__(self):
     self.tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
     self.synthesizer = self.tts.synthesizer
     self.config = self.synthesizer.tts_config
     self.sample_rate = self.synthesizer.tts_config.audio["sample_rate"]
+    self.max_sentence_length = 240
     self.speakers = {}
 
   def register_speaker(self, name, path):
@@ -30,7 +47,8 @@ class TTSModel:
     self.speakers[name] = { "gpt_cond_latent": gpt_cond_latent, "speaker_embedding": speaker_embedding}
 
   def split_into_sentences(self, text):
-    return self.synthesizer.split_into_sentences(text)
+    sentences = self.synthesizer.split_into_sentences(text)
+    return concatenate_to_segments(sentences, limit=self.max_sentence_length)
 
   def save_wav(self, wav, path):
     self.synthesizer.save_wav(wav=wav, path=path, pipe_out=None)
